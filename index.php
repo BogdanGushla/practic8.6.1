3f46664c3073750da3538932890e79d7b77709b3
BogdanGushla <121192545+BogdanGushla@users.noreply.github.com>
2022-12-22 04:06:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+ $p = 'Приветствую Вас на моей страничке!';
+?>
+<p>Zoom in <img src = "https://img2.freepng.ru/20180314/bkq/kisspng-computer-icons-grey-arrow-clip-art-curved-arrows-5aa9a2ece36745.8606345115210667329315.jpg" width="50" height="50"></p>
+ <?php
+ $name = 'Богдан';
+ $surname = 'Гушла';
+ $city = 'Ставрополь';
+ $age = 22;
+?>
+
+
+<?php
+include 'main.php';
+?>
---
+++ logo.inc.php
@@ -0,0 +1,3 @@
+ <div class="logo"> 
+                <img src="https://ie.wampi.ru/2022/12/21/php1.png" alt="php" width="350" height="350">
+                </div>     
